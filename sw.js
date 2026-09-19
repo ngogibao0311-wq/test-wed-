e7f5fffb6355f2fa533478a0635e89db69a66feb
ngogibao0311-wq <ngogibao0311@gmail.com>
2026-09-19 14:17:15 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const APP_VERSION = '4.0.3';
-const APP_BUILD = '2026.09.17.1-floating-toolbar-midautumn-a11y';
+const APP_BUILD = '2026.09.18.1-security-hotfix';
 const CACHE_VERSION = `study-shell-v${APP_VERSION}-${APP_BUILD}`;
 const OFFLINE_CACHE = `${CACHE_VERSION}-offline`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
